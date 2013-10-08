e47f842e1279182b987154a725147bb9403c78fe
iyst-01 <iyst-01@yandex.ru>
2013-10-08 07:45:03 +0300

    Test 1.1

--- TestIystRepository/src/test1/TestHWClass.java
+++ TestIystRepository/src/test1/TestHWClass.java
@@ -4,6 +4,7 @@ public class TestHWClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hellow World from iyst-01");
+		System.out.println("1.1 Hellow World from iyst-01");
 
 	}
 
