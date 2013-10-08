5096c78cba846cf734997fa71e6ec17175f384d6
iyst-01 <iyst-01@yandex.ru>
2013-10-08 22:26:58 +0300

    test dell row2

--- TestIystRepository/src/test1/TestHWClass.java
+++ TestIystRepository/src/test1/TestHWClass.java
@@ -5,9 +5,7 @@ public class TestHWClass {
 	public static void main(String[] args) {
 		System.out.println("Hellow World from iyst-01");
 		System.out.println("1.1 Hellow World from iyst-01");
-		System.out.println("1.2 Hellow World from iyst-01");
 		System.out.println("1.3 Hellow World from iyst-01");
-		System.out.println("1.4 Hellow World from iyst-01");
 		System.out.println("1.5 Hellow World from iyst-01");
 
 	}
